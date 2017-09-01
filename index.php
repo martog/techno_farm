8e572d507cf39e8dee63eae398b152985135f544
MARTIN\Martin <martin.holy.grigorov@gmail.com>
2017-09-01 23:41:46 +0300

    update

--- index.php
+++ index.php
@@ -10,24 +10,29 @@
 	<head>
 		<title>TechnoFarm</title>
 		<script type="text/javascript" src = "js/script.js"></script>	
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	</head>
 
 	<body>
 
 		<center>
 			<div id = "menu">
-				<button onclick = "show_contract_form()">Add contract</button><br>
-				<button onclick = "show_land_form()">Add land</button><br>
-				<button onclick = "show_landlord_form()">Add landlord</button><br>
-			</div>
+				<img src = "img/logo.jpg"><br>
+				<button onclick = "show_contract_form()" type="button" class="btn btn-outline-primary">Add contract</button>
+				<button onclick = "show_land_form()" type="button" class="btn btn-outline-primary">Add land</button>
+				<button onclick = "show_landlord_form()" type="button" class="btn btn-outline-primary">Add landlord</button>
+			</div><br><br>
 
-			<div id = "contract" style = "display: none;">
+			<div id = "contract" style = "display: none; width: 30%;">
 				<form method = "POST">
-					<input type = "text" name = "type" placeholder = "Type"><br>
-					<input type = "date" name = "start_date" placeholder = "Start date"><br>
-					<input type = "date" name = "end_date" placeholder = "End date"><br>
-					<input type = "number" step="0.01" name = "rent_per_decare" min = "0" placeholder = "Rent per decare"><br>
-					<input type = "number" step="0.01" name = "price" min = "0" placeholder = "Price"><br>
+					<input type = "text" class="form-control" name = "type" placeholder = "Type"><br>
+					<input type = "text" class="form-control" name = "start_date" onfocus="(this.type='date')" onblur="(this.type='text')" id="date" placeholder = "Start date"><br>
+					<input type = "text" class="form-control" name = "end_date" onfocus="(this.type='date')" onblur="(this.type='text')" id="date" placeholder = "End date"><br>
+					<input type = "number" class="form-control" step="0.01" name = "rent_per_decare" min = "0" placeholder = "Rent per decare"><br>
+					<input type = "number" class="form-control" step="0.01" name = "price" min = "0" placeholder = "Price"><br>
 					<input type="submit" name="contract">
 				</form>
 			</div>
